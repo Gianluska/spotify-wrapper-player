062c757842773415c5e8fc48d4785e558593d6c6
Gianlucca Claudino <gianluccaclaudino@gmail.com>
2020-03-21 05:44:06 -0300

    Adjusting package json

--- src/Spotify.js
+++ src/Spotify.js
@@ -1,7 +1,7 @@
 import SpotifyWrapper from 'spotify-wrapper';
 
 const spotify = new SpotifyWrapper({
-  token: 'BQA8hKzQoOUKUzjCRoWNkJK1YzHMtXAEkq5Zc_NWdDMAga4u3tx739XIp0VAmZ6AGkvPQ_ujpLmKPlTKgo0E5ZozIffyKf0Bu7RU0pCNpnQagLIeu5kSOWJrvY7r8_SfpbGU1G3dSp0fp4aERH-4iO1mg1qmoMlwNKrI8EwQ8-BpyBz6',
+  token: 'BQA6OBBnGVADK-dqyJDCshM_8vQBHEeBl5E1XLzMhCkXJ8H_5H0n2LfRs1P2Y6JhqdoXa3BAw1nIztg96lDnOkOcWGOETaVgR_wLuUGPavqJ7F1JdkjK4euV_PX2mrNS0nbIp49TMLWj19N-OdymYNuZySxqpjvm47-BVlogUYn9hoTu',
 });
 
 export default spotify;
