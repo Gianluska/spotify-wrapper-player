9c06b54982d44fca5579849299049d3832f12fd4
Gianlucca Claudino <gianluccaclaudino@gmail.com>
2020-03-21 05:21:53 -0300

    Adding methods SearchTrigger and SelectAlbumTrigger

--- src/main.js
+++ src/main.js
@@ -1,15 +1,7 @@
-import spotify from './Spotify';
-import renderAlbums from './AlbumList';
-import renderAlbumInfo from './AlbumInfo';
+import searchEnterTrigger from './SearchTrigger';
+import selectAlbumTrigger from './SelectAlbumTrigger';
+import playlistTrigger from './PlaylistTrigger';
 
-const albums = spotify.search.albums('Incubus');
-const albumList = document.getElementById('album-list');
-
-const album = spotify.album.getAlbum('6peEdPVO73WtgGah5sEhX4');
-const albumInfo = document.getElementById('album-info');
-
-albums
-  .then(data => renderAlbums(data.albums.items, albumList));
-
-album
-  .then(data => renderAlbumInfo(data, albumInfo));
+searchEnterTrigger();
+selectAlbumTrigger();
+playlistTrigger();
